ddd68190b5309cf381012c0f21a7f8455de00387
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-22 09:46:20 -0600

    initialized login form

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 
 // dependencies
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// set up body parser to parse url encoded form data from browser
+app.use(bodyParser.urlencoded({extended: false}));
 
 // set view engine
 app.set("view engine", "jade");
@@ -11,6 +15,15 @@ app.set("view engine", "jade");
 app.get("/", (req, res) =>
 {
   res.render("index");
+});
+app.get("/login", (req, res) =>
+{
+  res.render("login");
+});
+app.post("/login", (req, res) =>
+{
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.listen (PORT, () =>
