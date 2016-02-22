6979536629a1e8c3a85a748d585286b098e16547
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-22 11:17:11 -0600

    configuring redis

--- server.js
+++ server.js
@@ -26,8 +26,7 @@ app.use((req,res,next) =>
 {
   req.session.visits = req.session.visits || {};
    req.session.visits[req.url] = req.session.visits[req.url] || 0;
-   req.session.visits[req.url]++
-
+   req.session.visits[req.url]++;
    console.log(req.session);
    next();
 });
