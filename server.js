e65620c22a2ff99b7b386204b0171d21eb633d36
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-22 09:51:50 -0600

    initialized register page

--- server.js
+++ server.js
@@ -12,15 +12,29 @@ app.use(bodyParser.urlencoded({extended: false}));
 // set view engine
 app.set("view engine", "jade");
 
+// index page
 app.get("/", (req, res) =>
 {
   res.render("index");
 });
+
+// login routes
 app.get("/login", (req, res) =>
 {
   res.render("login");
 });
 app.post("/login", (req, res) =>
+{
+  console.log(req.body);
+  res.redirect("/");
+});
+
+// register routes
+app.get("/register", (req, res) =>
+{
+  res.render("register");
+});
+app.post("/register", (req, res) =>
 {
   console.log(req.body);
   res.redirect("/");
